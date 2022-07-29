5f28272cda6fca8e7272ec5366b0907d671ec5ee
DanteRamirezC <ramirezca207@gmail.com>
2022-07-28 19:48:41 -0700

    se reajustan ifs

--- codigo.js
+++ codigo.js
@@ -11,16 +11,28 @@ menuHamb.addEventListener("click", alternarMobileMenu);
 carShop.addEventListener("click", alternarProductCard)
 
 function alternarMobileMenu() {
+    const isCardClose = card.classList.contains("inactive__product-detail")
+    if(!isCardClose) {
+        card.classList.add("inactive__product-detail")
+    }
     mobileMenu.classList.toggle("inactive__mobile-menu");
 }
 function alternarDesktopMenu() {
+    const isCardClose = card.classList.contains("inactive__product-detail")
+    if(!isCardClose) {
+        card.classList.add("inactive__product-detail")
+    }
+ 
     deskTopMenu.classList.toggle("inactive");
 }
 function alternarProductCard() {
-    const isMobileClose = mobileMenu.classList.contains("inactive__mobile_menu");
-
+    const isDeskTopClose = card.classList.contains("inactive")
+    if(!isDeskTopClose) {
+        deskTopMenu.classList.add("inactive")
+    }
+    const isMobileClose = mobileMenu.classList.contains("inactive__mobile-menu");
     if(!isMobileClose) {
-        mobileMenu.classList.add("inactive__mobile_menu")
+        mobileMenu.classList.add("inactive__mobile-menu")
     }
 
     card.classList.toggle("inactive__product-detail")
