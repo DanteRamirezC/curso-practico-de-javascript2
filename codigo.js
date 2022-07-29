f92a0590db9d58b506e1f6faf137c7304af68c74
DanteRamirezC <ramirezca207@gmail.com>
2022-07-28 22:10:01 -0700

    se agrega las carsProducts

--- codigo.js
+++ codigo.js
@@ -3,7 +3,8 @@ const deskTopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuHamb = document.querySelector(".menu");
 const carShop = document.querySelector(".carrito");
-const card = document.querySelector(".product-detail")
+const card = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 
 navBarEmail.addEventListener("click", alternarDesktopMenu);
@@ -37,3 +38,57 @@ function alternarProductCard() {
 
     card.classList.toggle("inactive__product-detail")
 }
+// product-list //
+const productList = [];
+productList.push({
+    name: "bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "celular",
+    price: 1500,
+    image: "https://res.cloudinary.com/walmart-labs/image/upload/v1602113572/mg/images/categorias/d-celulares/category-smartphones.jpg"
+});
+productList.push({
+    name: "televisor",
+    price: 11000,
+    image: "https://images.samsung.com/is/image/samsung/p6pim/mx/un50au9000fxzx/gallery/mx-crystal-uhd-au9000-un50au9000fxzx-412358933?$720_576_PNG$"
+});
+
+function renderProducts(arr) {
+    for(product of productList) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+
+        const image = document.createElement("img");
+        image.setAttribute("src", product.image);
+
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+
+        const productInfoDiv = document.createElement("div");
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const figure = document.createElement("figure");
+        const figureImage = document.createElement("img");
+        figureImage.setAttribute("src", "./icons/bt_add_to_cart.svg")
+
+        figure.appendChild(figureImage);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(figure);
+
+        productCard.appendChild(image);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
